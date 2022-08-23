cbc2e49158a2837f746f8b403ca87e9d1063fa84
Fernando Mirabile <fernandom@mural.co>
2022-08-23 15:38:21 -0300

    Added netlify

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     preact(), 
-    react()
+    react(),
   ],
+  output: 'server',
+  adapter: netlify(),
 });
